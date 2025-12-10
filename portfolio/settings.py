744ee2ddffd13fa4247f3034713e4cda3faf6623
patriciaabogado12 <patriciaabogado10@gmail.com>
2025-12-11 00:30:58 +0800

    Add WhiteNoise to requirements and update settings for Render

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -125,8 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS = [ BASE_DIR / 'static' ]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_DIRS = [BASE_DIR / 'static']
+
 
 
